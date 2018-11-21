eb269f88a915c778c1fe156ad135c6dbfa0f096b
azorji kelechi oliver <kelechioliver96@gmail.com>
2018-11-21 04:28:10 +0100

    first app deployment

---
+++ public/precache-manifest.1efb413815f94e32266d588ed596be88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d0060613fb40b3288220",
+    "url": "/static/js/main.d0060613.chunk.js"
+  },
+  {
+    "revision": "916e4ad532338820642d",
+    "url": "/static/js/1.916e4ad5.chunk.js"
+  },
+  {
+    "revision": "d0060613fb40b3288220",
+    "url": "/static/css/main.ebce2224.chunk.css"
+  },
+  {
+    "revision": "5a6270d1ddd152568791ddb1111048a3",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema')
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
+// allow cross-origin request
+app.use(cors());
 app.use('/graphql',graphqlHTTP({
     schema,
     graphiql: true
 }));
+
+app.use(express.static('public'));
+app.get('*',(req,res) =>{
+    res.sendFile(path.resolve(_dirname,'public','index.html'))
+});
 
 const PORT = process.env.PORT || 5000;
 
